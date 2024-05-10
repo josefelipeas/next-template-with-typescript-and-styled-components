33dda5d2c52e2846de8fc46781b3cf33954bedad
José Felipe <devjosefelipe@gmail.com>
2024-05-10 13:47:15 -0400

    first commit

---
+++ src/styles/global-styles.ts
@@ -0,0 +1,10 @@
+'use client';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    background: red;
+  }
+`;
+
+export default GlobalStyles;
